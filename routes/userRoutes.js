b280f9bc5258c89b3bd02d127ab1eb046270275b
yaymo <stevenjamesonhill@gmail.com>
2018-08-30 20:50:47 -0400

    added initial crud routes for users

---
+++ routes/userRoutes.js
@@ -0,0 +1,35 @@
+const User = mongoose.model("users");
+
+module.exports = app => {
+  app.get("/api/users/:id", async (req, res) => {
+    const user = await User.findById(req.params.id);
+    res.send(user);
+  });
+
+  app.post("/api/users", async (req, res) => {
+    const { name, email, password, isAdmin } = req.body;
+
+    const user = new User({
+      name: { firstName: name.firstName, lastName: name.lastName },
+      email,
+      password,
+      isAdmin
+    });
+
+    try {
+      await user.save();
+      res.send(user);
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  });
+
+  app.delete("/api/users/:id", async (req, res) => {
+    try {
+      await User.findByIdAndRemove({ _id: req.params.id }).exec();
+      res.send(200);
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  });
+};
