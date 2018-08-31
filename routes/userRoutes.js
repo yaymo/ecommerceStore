6d98fc48aea6e6eda9a0628ce12d3ec253726d1b
yaymo <stevenjamesonhill@gmail.com>
2018-08-30 21:01:43 -0400

    added mongo dev config

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const User = mongoose.model("users");
 
 module.exports = app => {
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+mongoose.connect(keys.mongoURI);
 
 require("./models/User");
 require("./models/Item");
+require("./routes/userRoutes")(app);
 
 app.use(bodyParser.json());
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
