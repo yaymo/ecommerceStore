a9ff131277caa0fb3c9927a21c76cebba76232f9
yaymo <stevenjamesonhill@gmail.com>
2018-09-02 20:43:12 -0400

    move passport logic to a separate service

--- server.js
+++ server.js
@@ -2,48 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const { User } = require("./models/User");
-
 const keys = require("./config/keys");
+
+require("./models/User");
+require("./models/Item");
+require("./services/passport");
 
 mongoose.connect(
   keys.mongoURI,
   { useNewUrlParser: true }
 );
-passport.use(
-  new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-    User.findOne({ email: email }, (err, user) => {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      if (!user.validatePassword(password)) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
-    });
-  })
-);
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
 
-passport.deserializeUser(function(id, cb) {
-  User.findById(id, function(err, user) {
-    cb(err, user);
-  });
-});
-
-require("./models/User");
-require("./models/Item");
 require("./routes/userRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
