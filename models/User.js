93f2db683c5329e771a375cfc7aa529eae68b437
yaymo <stevenjamesonhill@gmail.com>
2018-09-02 20:31:27 -0400

    added local authentication

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   firstName: String,
@@ -8,4 +9,22 @@ const userSchema = new Schema({
   password: { type: String, required: true },
   isAdmin: { type: Boolean, required: true }
 });
-mongoose.model("users", userSchema);
+
+userSchema.methods.apiResp = user => {
+  return {
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    isAdmin: user.isAdmin,
+    _id: user._id
+  };
+};
+
+userSchema.methods.validatePassword = (password, hash) => {
+  return bcrypt.compare(password, hash).then(isValid => isValid);
+};
+
+userSchema.statics.hashPassword = password => {
+  return bcrypt.hash(password, 10).then(hash => hash);
+};
+mongoose.model("user", userSchema);
--- server.js
+++ server.js
@@ -1,15 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const keys = require("./config/keys");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { User } = require("./models/User");
 
-const app = express();
-app.use(bodyParser.json());
+const keys = require("./config/keys");
 
 mongoose.connect(
   keys.mongoURI,
   { useNewUrlParser: true }
 );
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+    User.findOne({ email: email }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (!user.validatePassword(password)) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    });
+  })
+);
+const app = express();
+
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  User.findById(id, function(err, user) {
+    cb(err, user);
+  });
+});
 
 require("./models/User");
 require("./models/Item");
